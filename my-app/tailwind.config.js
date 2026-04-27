79acdbc3050022afb61117fb3cc97c2d200842fa
PenitentOne-2005 <pozdrakovzena@gmail.com>
2026-04-27 22:59:58 +0300

    update config

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -3,21 +3,25 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: ["./App.tsx", "./my-app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.tsx",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    colors: {
-      primary: "#BF3335",
-      gray: {
-        DEFAULT: "#282828",
-        500: "#1D1D1D",
-      },
-      black: "#030207",
-      white: colors.white,
-      trasparent: colors.trasparent,
-      red: colors.red["500"],
-    },
     extend: {
+      colors: {
+        primary: "#BF3335",
+        gray: {
+          DEFAULT: "#282828",
+          500: "#1D1D1D",
+        },
+        black: "#030207",
+        white: colors.white,
+        transparent: colors.transparent,
+        red: colors.red["500"],
+      },
       zIndex: {
         1: "1",
       },
