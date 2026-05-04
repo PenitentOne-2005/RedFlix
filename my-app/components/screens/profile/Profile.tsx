720c0a5f2664b4d6725b4ade490cea9a024ddc08
PenitentOne-2005 <pozdrakovzena@gmail.com>
2026-05-04 22:00:22 +0300

    add logout

--- my-app/components/screens/profile/Profile.tsx
+++ my-app/components/screens/profile/Profile.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { Pressable, Text, View } from "react-native";
+import { logout } from "@/providers/auth/authLogout";
 
 const Profile = () => {
   return (
     <View>
-      <Text>Profile</Text>
+      <Pressable
+        onPress={logout}
+        className="opacity-40 items-center flex-row justify-end"
+      >
+        <AntDesign name={"logout"} size={18} color="white" />
+        <Text className="text-white text-lg ml-2">Logout</Text>
+      </Pressable>
     </View>
   );
 };
